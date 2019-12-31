1f20b1679f02fb707278a0369415d30208a81834
Pikles <11828103+myleskeeffe@users.noreply.github.com>
2019-12-31 12:35:41 +0800

    Update stats.php

--- results/stats.php
+++ results/stats.php
@@ -5,56 +5,17 @@ header('Content-Type: text/html; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
+$ServiceName = "Example Service";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>LibreSpeed - Stats</title>
-<style type="text/css">
-	html,body{
-		margin:0;
-		padding:0;
-		border:none;
-		width:100%; min-height:100%;
-	}
-	html{
-		background-color: hsl(198,72%,35%);
-		font-family: "Segoe UI","Roboto",sans-serif;
-	}
-	body{
-		background-color:#FFFFFF;
-		box-sizing:border-box;
-		width:100%;
-		max-width:70em;
-		margin:4em auto;
-		box-shadow:0 1em 6em #00000080;
-		padding:1em 1em 4em 1em;
-		border-radius:0.4em;
-	}
-	h1,h2,h3,h4,h5,h6{
-		font-weight:300;
-		margin-bottom: 0.1em;
-	}
-	h1{
-		text-align:center;
-	}
-	table{
-		margin:2em 0;
-		width:100%;
-	}
-	table, tr, th, td {
-		border: 1px solid #AAAAAA;
-	}
-	th {
-		width: 6em;
-	}
-	td {
-		word-break: break-all;
-	}
-</style>
+<title><?php echo $ServiceName;  ?></title>
+<link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
-<h1>LibreSpeed - Stats</h1>
+<div class="jumbotron">
+<div style="text-align: center"><h1><?php echo $ServiceName; ?></h1></div>
 <?php
 include_once("telemetry_settings.php");
 require "idObfuscation.php";
@@ -80,14 +41,32 @@ if($stats_password=="PASSWORD"){
 			$conn = new PDO("pgsql:$conn_host;$conn_db;$conn_user;$conn_password");
 		}else die();
 ?>
-	<form action="stats.php" method="GET"><input type="hidden" name="op" value="logout" /><input type="submit" value="Logout" /></form>
+	<form action="stats.php" method="GET"><input type="hidden" name="op" value="logout" /><input class="btn btn-dark" style="position: absolute; top: 20px; right: 20px;" type="submit" value="Logout" /></form>
 	<form action="stats.php" method="GET">
 		<h3>Search test results</h6>
+		<div class="btn-group">
 		<input type="hidden" name="op" value="id" />
-		<input type="text" name="id" id="id" placeholder="Test ID" value=""/>
-		<input type="submit" value="Find" />
-		<input type="submit" onclick="document.getElementById('id').value=''" value="Show last 100 tests" />
+		<input class="input-group"  type="text" name="id" id="id" placeholder="Test ID" value=""/>
+		<input class="btn btn-dark" type="submit" value="Find" />
+		<input class="btn btn-dark" type="submit" onclick="document.getElementById('id').value=''" value="Show last 100 tests" />
+		</div>
 	</form>
+</div>
+	<table class="table">
+                <thead>
+                        <tr>
+                                <th scope="col">Test ID</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">IP/ISP</th>
+                                <th scope="col">User Agent</th>
+                                <th scope="col">Down Speed</th>
+                                <th scope="col">Up Speed</th>
+                                <th scope="col">Ping</th>
+                                <th scope="col">Jitter</th>
+                                <th scope="col">Log</th>
+                                <th scope="col">Extra</th>
+                        </tr>
+                </thead>
 	<?php
 		$q=null;
 		if($_GET["op"]=="id"&&!empty($_GET["id"])){
@@ -110,7 +89,7 @@ if($stats_password=="PASSWORD"){
 				$q->store_result();
 				$q->bind_result($id,$timestamp,$ip,$ispinfo,$ua,$lang,$dl,$ul,$ping,$jitter,$log,$extra);
 			} else if($db_type=="sqlite"||$db_type=="postgresql"){
-				$q=$conn->prepare("select id,timestamp,ip,ispinfo,ua,lang,dl,ul,ping,jitter,log,extra from speedtest_users order by timestamp desc limit 100");
+				$q=$conn->prepare("select id,timestamp,ip,ispinfo,ua,lang,dl,ul,ping,jitter,log,extra from speedtest_users order by timestamp desc limit 0,100");
 				$q->execute();
 			}else die();
 		}
@@ -134,21 +113,22 @@ if($stats_password=="PASSWORD"){
 				$extra=$row["extra"];
 			}else die();
 	?>
-		<table>
-			<tr><th>Test ID</th><td><?=htmlspecialchars(($enable_id_obfuscation?(obfuscateId($id)." (deobfuscated: ".$id.")"):$id), ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Date and time</th><td><?=htmlspecialchars($timestamp, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>IP and ISP Info</th><td><?=$ip ?><br/><?=htmlspecialchars($ispinfo, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>User agent and locale</th><td><?=$ua ?><br/><?=htmlspecialchars($lang, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Download speed</th><td><?=htmlspecialchars($dl, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Upload speed</th><td><?=htmlspecialchars($ul, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Ping</th><td><?=htmlspecialchars($ping, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Jitter</th><td><?=htmlspecialchars($jitter, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Log</th><td><?=htmlspecialchars($log, ENT_HTML5, 'UTF-8') ?></td></tr>
-			<tr><th>Extra info</th><td><?=htmlspecialchars($extra, ENT_HTML5, 'UTF-8') ?></td></tr>
-		</table>
+			<tr>
+			<td><?=htmlspecialchars(($enable_id_obfuscation?(obfuscateId($id)." (deobfuscated: ".$id.")"):$id), ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($timestamp, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=$ip ?><br/><?=htmlspecialchars($ispinfo, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=$ua ?><br/><?=htmlspecialchars($lang, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($dl, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($ul, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($ping, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($jitter, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($log, ENT_HTML5, 'UTF-8') ?></td>
+			<td><?=htmlspecialchars($extra, ENT_HTML5, 'UTF-8') ?></td>
+			</tr>
 	<?php
 		}
 	?>
+			</table>
 <?php
 	}
 }else{
